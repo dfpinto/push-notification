4ee1022d321db8b3c889e5dbf3a09e7b6b317054
dfpinto <dirley.figueredo@gmail.com>
2022-06-19 14:11:54 -0300

    Sending push notification from back end.

---
+++ backend/src/test/java/com/devsuperior/pocfmc/PocfmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.pocfmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocfmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
